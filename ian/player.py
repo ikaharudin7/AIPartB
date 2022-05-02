ab4eff6d5c17c1f583c3153073634ccefa1d54ca
ikaharudin7 <ikaharudin7@gmail.com>
2022-05-02 21:17:12 +1000

    First turn done

--- ian/player.py
+++ ian/player.py
@@ -1,6 +1,11 @@
 
+from cmath import inf
+from collections import defaultdict
+
+from numpy import ceil
 from referee.board import Board
 
+STEP_SIZE = 1
 
 class Player:
     def __init__(self, player, n):
@@ -14,6 +19,10 @@ class Player:
         """
         # put your code here
         self.colour = player
+        self.moves = 0
+        Board.n = n
+
+
 
 
 
@@ -23,7 +32,19 @@ class Player:
         of the game, select an action to play.
         """
         # put your code here
-    
+        self.moves += 1
+
+        # First move option
+        if self.moves == 1 and self.colour == "red":
+            if Board.n % 2 == 0:
+                return ("PLACE", int(ceil(Board.n / 2)), int(ceil(Board.n / 2)))
+            else: 
+                return ("PLACE", int(ceil(Board.n / 2 - 1)), int(ceil(Board.n / 2)))
+        # Second move option
+        elif self.moves == 1 and self.colour == "blue":
+            return ("STEAL", )
+        
+
     def turn(self, player, action):
         """
         Called at the end of each player's turn to inform this player of 
@@ -36,4 +57,144 @@ class Player:
         above. However, the referee has validated it at this point.
         """
         # put your code here
+        print(Board.is_occupied)
 
+        self.board = []
+
+        # Keep track of board in array form like part A, with occupied tuples there, with the colour. 
+
+        return action
+
+
+# A star algorithm
+def aStar(start, goal, size, board):
+    # Keep track of which hexes have been blocked
+    blocked = []
+    for hex in board:
+        blocked.append(hex)
+
+    start = (start[0], start[1])
+    goal = (goal[0], goal[1])
+    traversed = []
+
+    # Define set of hexagons who's children have not been fully traversed
+    open = set()
+    open.add(start)
+
+    descended_from = {}
+    
+    # Dictionaries defining a node and its gn and fn values
+    g_cost = {}
+    g_cost[start] = STEP_SIZE
+    function = {}
+    function[start] = calc_heuristic(start, goal) + g_cost[start]
+
+    while len(open) > 0:
+        # Get the node in open with the minimum fn score.
+        curr = min_fn(open, function)
+        blocked.append(curr)
+
+        # If the goal has been found and optimised, print out solution
+        if curr == goal:
+            print(g_cost.get(goal))
+            return get_history(descended_from, curr, start)
+
+        open.remove(curr)
+        for neighbour in generate_branch(curr, size, blocked):
+            curr_dist = g_cost[curr] + STEP_SIZE
+            # If neighbour hasn't been seen yet, set it's g_cost to infinity
+            if neighbour not in traversed:
+                g_cost[neighbour] = inf
+                traversed.append(neighbour)
+                
+
+            # If the distance calculated is lower than its previous cost...
+            if curr_dist < g_cost[neighbour]:
+                descended_from[neighbour] = curr
+                g_cost[neighbour] = curr_dist
+                function[neighbour] = curr_dist + calc_heuristic(neighbour, goal)
+
+                if neighbour not in open:
+                    open.add(neighbour)
+    # If no solution is possible then return failure
+    return 0
+
+# Builds the path 
+def get_history(descended_from, curr, start):
+    soln = [curr]
+    # Iterate through the descended dictionary
+    while curr in list(descended_from.keys()):
+        curr = descended_from[curr]
+        soln.append(curr)
+    return soln
+    
+
+# Calculates the start and goal by taking in the coordinates as lists 
+# in [r, q] format.
+def calc_heuristic(start, goal):
+    r1 = start[0]
+    q1 = start[1]
+    r2 = goal[0]
+    q2 = goal[1]
+
+    # If heading top right or bottom left direction, use Manhattan distance
+    if (q2 >= q1 and r2 >= r1 or q2 <= q1 and r2 <= r1):
+        prediction = abs(q2 - q1) + abs(r2 - r1)
+    # If goal is to the left of the start...
+    else:
+        prediction = max(abs(q2 - q1), abs(r2 - r1))
+
+    return prediction
+
+
+# Generate the possible branches from that node
+def generate_branch(curr, size, blocked):
+    branches = []
+    max = size
+    min = 0
+
+    r1 = curr[0]
+    q1 = curr[1]
+
+    # Add possible branches into the branches list (Just brute force as there are only 6)
+    if r1 - 1 >= min and (r1 - 1, q1) not in blocked:
+        branches.append((r1 - 1, q1))
+
+    if r1 + 1 < max  and (r1 + 1, q1) not in blocked:
+        branches.append((r1 + 1, q1))
+    
+    if q1 - 1 >= min and (r1, q1 - 1) not in blocked:
+        branches.append((r1, q1 - 1))
+    
+    if q1 + 1 < max and (r1, q1 + 1) not in blocked:
+        branches.append((r1, q1 + 1))
+
+    if q1 + 1 < max and r1 - 1 >= min and (r1 - 1, q1 + 1) not in blocked:
+        branches.append((r1 - 1, q1 + 1))
+
+    if q1 - 1 >= min and r1 + 1 < max and (r1 + 1, q1 - 1) not in blocked:
+        branches.append((r1 + 1, q1 - 1))
+
+    return branches
+
+# Takes the data and transforms the board into a dictionary with keys
+# as the tuple, and colour as the value. 
+def board_dict(data):
+    board = defaultdict()
+
+    # Iterate through list and assign dictionary values
+    for list in data.get("board"):
+        tuple = (list[1], list[2])
+        board[tuple] = list[0]
+    return board
+
+# Get node in openset with smallest fn value
+def min_fn(openset, function):
+    scores = []
+    # Iteratte through the openset
+    for hex in openset:
+        scores.append((function[hex], hex))
+    
+    scores.sort(key=lambda y: y[0])
+
+    return scores[0][1]
